1787f587a5d4bb795fb7dbed9c202577c1da0af2
Uzurch1k <galcenkovlad7@gmail.com>
2024-08-26 03:18:35 +0300

    fix

--- src/js/parts/navigation.js
+++ src/js/parts/navigation.js
@@ -3,6 +3,8 @@ const mobMenu = document.querySelector('.mob');
 // const navBtns = document.querySelectorAll('.nav-menu__btn_mob');
 // const navLinks = document.querySelectorAll('.nav-menu__link');
 const headerSoc = document.querySelector('.header__soc');
+const headerBtnMob = document.querySelector('.header__btn-mob');
+const logoDef = document.querySelector('.logo-def');
 const body = document.querySelector('body');
 
 function updateMobMenuBodyMargin() {
@@ -19,6 +21,8 @@ function toggleMenu() {
   burger.classList.toggle('is-opened');
   mobMenu.classList.toggle('is-opened');
   headerSoc.classList.toggle('is-opened');
+  headerBtnMob.classList.toggle('is-opened');
+  logoDef.classList.toggle('is-opened');
   body.classList.toggle('overhide');
 }
 
@@ -26,6 +30,8 @@ function closeMenu() {
   burger.classList.remove('is-opened');
   mobMenu.classList.remove('is-opened');
   headerSoc.classList.remove('is-opened');
+  headerBtnMob.classList.remove('is-opened');
+  logoDef.classList.remove('is-opened');
   body.classList.remove('overhide');
 }
 
