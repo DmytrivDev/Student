33b7f91f98b4ee4293b13cb4be4b7e39ce11294c
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-09-30 15:04:21 +0300

    вм

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import './parts/splide';
 import './parts/items-progress';
 import './parts/accordion';
 import './parts/reviews.js';
+import './parts/scroll.js';
 
 initMenu();
 initPopupMenu();
---
+++ src/js/parts/scroll.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.btn-scroll').forEach(button => {
+    button.addEventListener('click', function() {
+        const targetId = this.getAttribute('data-id');
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
