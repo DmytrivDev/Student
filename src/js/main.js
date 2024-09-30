2839ad9232501fc8b86014c803f09142cabfccca
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-09-30 12:29:54 +0300

    H2

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import { initMenu, initPopupMenu } from './parts/navigation';
-import './parts/header-scroll.js';
 import './parts/splide';
 import './parts/items-progress';
 import './parts/accordion';
